d3903185a6bd163629b26080c52b73173fe23542
TheXenOne <benlandor@live.com>
2025-11-22 13:09:59 +0000

    Fix auth0 whitelist redirection.
    Signed-off-by: TheXenOne <benlandor@live.com>

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,17 @@
-// src/proxy.ts
+import { NextResponse } from "next/server";
 import { auth0 } from "./lib/auth0";
 
 export async function proxy(request: Request) {
+    const url = new URL(request.url);
+
+    if (url.pathname === "/auth/callback") {
+        const err = url.searchParams.get("error");
+        if (err === "access_denied") {
+            // just logout and return home (no query string)
+            return NextResponse.redirect(new URL("/auth/logout", url.origin));
+        }
+    }
+
     return await auth0.middleware(request);
 }
 
